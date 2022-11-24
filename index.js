0b790202a7ece45b36a1d5b2fee9f7e466a0222c
Ravi <ravivyas922@gmail.com>
2022-11-23 21:10:09 -0500

    commit tree b66a4b9f3e26a76d951c18fece577a7b35d3d34e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1=31;
+const num2=2
+const num3 = 19;
+const num4 = 5;
+let multiply = num1*num2;
+let random = Math.ceil(Math.random()*10);
+let mod = num3%num4;
+let max = Math.max(1,2,3,4,20);
+console.log(multiply);
+console.log(random);
+console.log(mod);
+console.log(max);
